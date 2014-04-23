034f1b432ba9c2f98c51d1ededa904b933968e07
J Baker <rekab@hindenburg.org>
2014-04-22 20:31:26 -0700

    remove hello_world stuff

--- gen/com/example/stfu/R.java
+++ gen/com/example/stfu/R.java
@@ -87,13 +87,12 @@ public final class R {
         public static final int app_picker_list_item=0x7f030003;
         public static final int bookmark_picker_list_item=0x7f030004;
         public static final int encode=0x7f030005;
-        public static final int fragment_main=0x7f030006;
-        public static final int help=0x7f030007;
-        public static final int history_list_item=0x7f030008;
-        public static final int live_card_layout=0x7f030009;
-        public static final int search_book_contents=0x7f03000a;
-        public static final int search_book_contents_header=0x7f03000b;
-        public static final int search_book_contents_list_item=0x7f03000c;
+        public static final int help=0x7f030006;
+        public static final int history_list_item=0x7f030007;
+        public static final int live_card_layout=0x7f030008;
+        public static final int search_book_contents=0x7f030009;
+        public static final int search_book_contents_header=0x7f03000a;
+        public static final int search_book_contents_list_item=0x7f03000b;
     }
     public static final class menu {
         public static final int capture=0x7f0c0000;
@@ -108,7 +107,7 @@ public final class R {
         /**  Actions 
          */
         public static final int action_open_browser=0x7f0a0078;
-        public static final int action_scan=0x7f0a007d;
+        public static final int action_scan=0x7f0a007c;
         public static final int action_settings=0x7f0a007a;
         public static final int action_share=0x7f0a0079;
         public static final int app_name=0x7f0a0000;
@@ -143,7 +142,6 @@ public final class R {
         public static final int contents_phone=0x7f0a001d;
         public static final int contents_sms=0x7f0a001e;
         public static final int contents_text=0x7f0a001f;
-        public static final int hello_world=0x7f0a007c;
         public static final int history_clear_one_history_text=0x7f0a0021;
         public static final int history_clear_text=0x7f0a0020;
         public static final int history_email_title=0x7f0a0022;
